ed1dc4f874fa162611b5c11184772f6a247bb751
umut <35867379+hopethewarior@users.noreply.github.com>
2018-05-05 16:51:34 +0300

    Revert "Hopetheslayer"
    This reverts commit d83f853dc1ae5cc4a260e38cb8aa6d87786d442b.

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Sistemden Mail atmak için
-Route::get('contact','ContactController@Show');
-Route::post('contact','ContactController@mail');
